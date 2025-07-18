afcb870e944630c5720e1a45d71f95cbbd53810e
Your Name <meejanursk@gmail.com>
2025-07-18 19:55:35 +0530

    dfdf

--- mzco/src/components/Bg.jsx
+++ mzco/src/components/Bg.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React , {useContext} from 'react'
 import {useTheme} from '../context/ThemeContext.jsx'
+import { IsMobileContext } from '../context/IsMobile.jsx'
 function Bg() {
+  const {isMobile } = useContext(IsMobileContext)
+  console.log(isMobile)
 const {isDarkMode} =  useTheme()
   return (
     <div   className='fixed top-0 pointer-events-none z-[0] h-screen w-screen bg-white  flex justify-center items-center'>
-    <div style={{scale : isDarkMode ? 3 : 0}} className='rounded-full transition-transform duration-1000 h-[50vw] w-[50vw] bg-black'></div>
+    <div style={{scale : isDarkMode ? ( isMobile ? 6 : 3) : 0}} className='rounded-full transition-transform duration-1000 h-[50vw] w-[50vw] bg-black'></div>
     </div>
   )
 }
